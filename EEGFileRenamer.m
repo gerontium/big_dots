a31de0cc64c9d39bf431be4ca6f23d28e9530fdc
DanielPNewman <dan.newman86@gmail.com>
2016-06-07 16:39:10 +1000

    Finished with EEGFileRenamer.m :-)

--- EEGFileRenamer.m
+++ EEGFileRenamer.m
@@ -56,139 +56,136 @@ close all
 
 %----SET PARAMETERS HERE----
 path = 'S:\R-MNHS-SPP\Bellgrove-data\4. Dan Newman\Participant Folders_new\';
-
 subject_folder = {'279M_FT','234M_SW','484M_AI','453M_LB'};
-%Insert here the main folder
-%for example 'G:\DirectionDots_data\DD2M\', the script will search
-%for mistakes in this folder.
 
 for s=1:length(subject_folder)
-    datafolder = 'G:\DirectionDots_data\DD2M\';
-    
-    
-    %Insert here the wrong names (no file extension needed !) as a list (cell)
-    %for example {'DDM2eeg','DD2M3.eg'}
-    wrongnames={'FT34M1eeg','SW5010', 'AI58MS1', 'AI58MS2', 'AI58MS3', 'AI58MS4',...
-        'AI58MS5', 'AI58MS6', 'AI58MS7', 'AI58MS8', 'AI58MS9', 'AI58MS10',...
-        'AI58MS11', 'AI58MS12','AI58MS13', 'AI58MS14', 'AI58MS15', 'AI58MS16',...
-        'LB59M12','LB59M12b'};
-    
-    %Insert here the right names (no file extension needed !, again ? - Yes, again !)
-    % as a list (cell).
-    %for example {'DDM2','DD2M3'}
-    rightnames={'FT34M1','SW50M10','AI58M1', 'AI58M2', 'AI58M3', 'AI58M4',...
-        'AI58M5', 'AI58M6', 'AI58M7', 'AI58M8', 'AI58M9', 'AI58M10',...
-        'AI58M11', 'AI58M12','AI58M13', 'AI58M14', 'AI58M15', 'AI58M16',...
-        'LB59M11','LB59M12'}; %
-    %----MAIN SCRIPT----
-    
-    
-    %---CREATE NAME CHANGING LOG FILE ---
-    logfid=fopen([datafolder 'NameChangeLog.txt'],'w');
-    
-    %Write the Mainfolder in name changing log file
-    fprintf(logfid,'Mainfolder: %s \n\n',datafolder);
-    
-    %Correct name for each wrong named file
-    for k=1:length(wrongnames)
-        
-        %---WRONGFILE READING---
-        
-        %-VHDR-%
-        %wrongfile
-        wrongfile_vhdr=[wrongnames{k} '.vhdr'];
-        
-        %open wrong file
-        wrongfid_vhdr=fopen([datafolder wrongfile_vhdr],'r');
-        
-        %Read first part of the file, before mistaken names
-        firstpart=fread(wrongfid_vhdr,134,'uint8');
-        
-        %jump wrong name part of the file
-        fseek(wrongfid_vhdr,length(wrongnames{k})+4+13+length(wrongnames{k})+5,'cof');
-        
-        
-        %Read second part of the file, after mistaken names
-        secondpart=fread(wrongfid_vhdr,inf,'uint8');
-        
-        %Close wrongfile
-        fclose(wrongfid_vhdr);
-        
-        %-VMRK-%
-        wrongfile_vmrk=[wrongnames{k} '.vmrk'];
-        
-        %open wrong file
-        wrongfid_vmrk=fopen([datafolder wrongfile_vmrk],'r');
-        
-        %Read first part of the file, before mistaken names
-        firstpart_vmrk=fread(wrongfid_vmrk,96,'uint8');
-        
-        %jump wrong name part of the file
-        fseek(wrongfid_vmrk,length(wrongnames{k})+4+2,'cof');
-        
-        %Read second part of the file, after mistaken names
-        secondpart_vmrk=fread(wrongfid_vmrk,inf,'uint8');
-        
-        %Close wrongfile
-        fclose(wrongfid_vmrk);
-        
-        
-        %---RIGHTFILE WRITING---
-        
-        %-VHDR-%
-        %Create right file
-        rightfid_vhdr=fopen([datafolder rightnames{k} '.vhdr'],'wb');
-        
-        %Write first part of the file
-        fwrite(rightfid_vhdr,firstpart,'uint8');
-        
-        %Concatenate right names
-        string_vhdr=[rightnames{k} '.eeg' char([13,10]) 'MarkerFile=' rightnames{k} '.vmrk'];
-        fwrite(rightfid_vhdr,string_vhdr,'schar');
-        
-        %Write second part of the file
-        fwrite(rightfid_vhdr,secondpart,'uint8');
-        
-        %Close right file
-        fclose(rightfid_vhdr);
-        
-        
-        %-VMRK-%
-        %Create right file
-        rightfid_vmrk=fopen([datafolder rightnames{k} '.vmrk'],'wb');
-        
-        %Write first part of the file
-        fwrite(rightfid_vmrk,firstpart_vmrk,'uint8');
-        
-        %Concatenate right names
-        string_vmrk=[rightnames{k} '.eeg' char([13,10])];
-        fwrite(rightfid_vmrk,string_vmrk,'schar');
-        
-        %Write second part of the file
-        fwrite(rightfid_vmrk,secondpart_vmrk,'uint8');
-        
-        %Close right file
-        fclose(rightfid_vmrk);
-        
-        %---RENAMING EEG FILE---
-        movefile([datafolder wrongnames{k} '.eeg'],[datafolder rightnames{k} '.eeg']);
-        
-        %---DELETING OLD FILES---
-        delete([datafolder wrongnames{k} '.vhdr']);
-        delete([datafolder wrongnames{k} '.vmrk']);
-        
-        
-        %write name changes in the log file
-        fprintf(logfid,'%s to %s\n',wrongnames{k},rightnames{k});
-        
-        
-        
+    datafolder = [path, subject_folder{s}, '\'];
+    %%
+    clear wrongnames rightnames
+    if strcmp(subject_folder{s},'279M_FT')
+        wrongnames={'FT34M1eeg'};
+        rightnames={'FT34M1'};
+    elseif strcmp(subject_folder{s},'234M_SW')
+        wrongnames={'SW5010'};
+        rightnames={'SW50M10'};
+    elseif strcmp(subject_folder{s},'484M_AI')
+        wrongnames={'AI58MS1','AI58MS2', 'AI58MS3', 'AI58MS4',...
+            'AI58MS5', 'AI58MS6', 'AI58MS7', 'AI58MS8', 'AI58MS9', 'AI58MS10',...
+            'AI58MS11', 'AI58MS12','AI58MS13', 'AI58MS14', 'AI58MS15', 'AI58MS16'};
+        rightnames={'AI58M1', 'AI58M2', 'AI58M3', 'AI58M4',...
+            'AI58M5', 'AI58M6', 'AI58M7', 'AI58M8', 'AI58M9', 'AI58M10',...
+            'AI58M11', 'AI58M12','AI58M13', 'AI58M14', 'AI58M15', 'AI58M16'}; %
+    elseif strcmp(subject_folder{s},'453M_LB')
+        wrongnames={'LB59M12','LB59M12b'};
+        rightnames={'LB59M11','LB59M12'};
     end
+    %%
+%----MAIN SCRIPT----
+
+
+%---CREATE NAME CHANGING LOG FILE ---
+logfid=fopen([datafolder 'NameChangeLog.txt'],'w');
+
+%Write the Mainfolder in name changing log file
+fprintf(logfid,'Mainfolder: %s \n\n',datafolder);
+
+%Correct name for each wrong named file
+for k=1:length(wrongnames)
+
+    %---WRONGFILE READING---
+
+    %-VHDR-%
+    %wrongfile
+    wrongfile_vhdr=[wrongnames{k} '.vhdr'];
+
+    %open wrong file
+    wrongfid_vhdr=fopen([datafolder wrongfile_vhdr],'r');
+
+    %Read first part of the file, before mistaken names
+    firstpart=fread(wrongfid_vhdr,134,'uint8');
+
+    %jump wrong name part of the file
+    fseek(wrongfid_vhdr,length(wrongnames{k})+4+13+length(wrongnames{k})+5,'cof');
+
+
+    %Read second part of the file, after mistaken names
+    secondpart=fread(wrongfid_vhdr,inf,'uint8');
+
+    %Close wrongfile
+    fclose(wrongfid_vhdr);
+
+    %-VMRK-%
+    wrongfile_vmrk=[wrongnames{k} '.vmrk'];
+
+    %open wrong file
+    wrongfid_vmrk=fopen([datafolder wrongfile_vmrk],'r');
+
+    %Read first part of the file, before mistaken names
+    firstpart_vmrk=fread(wrongfid_vmrk,96,'uint8');
+
+    %jump wrong name part of the file
+    fseek(wrongfid_vmrk,length(wrongnames{k})+4+2,'cof');
+
+    %Read second part of the file, after mistaken names
+    secondpart_vmrk=fread(wrongfid_vmrk,inf,'uint8');
+
+    %Close wrongfile
+    fclose(wrongfid_vmrk);
+
+
+    %---RIGHTFILE WRITING---
+
+    %-VHDR-%
+    %Create right file
+    rightfid_vhdr=fopen([datafolder rightnames{k} '.vhdr'],'wb');
+
+    %Write first part of the file
+    fwrite(rightfid_vhdr,firstpart,'uint8');
+
+    %Concatenate right names
+    string_vhdr=[rightnames{k} '.eeg' char([13,10]) 'MarkerFile=' rightnames{k} '.vmrk']; 
+    fwrite(rightfid_vhdr,string_vhdr,'schar');
+
+    %Write second part of the file
+    fwrite(rightfid_vhdr,secondpart,'uint8');
+
+    %Close right file
+    fclose(rightfid_vhdr);
+
+
+    %-VMRK-%
+    %Create right file
+    rightfid_vmrk=fopen([datafolder rightnames{k} '.vmrk'],'wb');
+
+    %Write first part of the file
+    fwrite(rightfid_vmrk,firstpart_vmrk,'uint8');
+
+    %Concatenate right names
+    string_vmrk=[rightnames{k} '.eeg' char([13,10])];
+    fwrite(rightfid_vmrk,string_vmrk,'schar');
+
+    %Write second part of the file
+    fwrite(rightfid_vmrk,secondpart_vmrk,'uint8');
+
+    %Close right file
+    fclose(rightfid_vmrk);
+
+    %---RENAMING EEG FILE---
+    movefile([datafolder wrongnames{k} '.eeg'],[datafolder rightnames{k} '.eeg']);
+
+    %---DELETING OLD FILES---
+    delete([datafolder wrongnames{k} '.vhdr']);
+    delete([datafolder wrongnames{k} '.vmrk']);
+    
+    
+    %write name changes in the log file
+    fprintf(logfid,'%s to %s\n',wrongnames{k},rightnames{k});
+    
+    
+    
+end
 end
 fprintf('Done !\n');
 
 %Close Name change log file
 fclose(logfid);
-
-
-
