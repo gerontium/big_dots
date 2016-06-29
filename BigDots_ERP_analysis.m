004bd627bd5922c141f84237d677485b32c52ff3
DanielPNewman <dan.newman86@gmail.com>
2016-06-29 10:47:04 +1000

    just added Monash path_temp to  runafew_BigDots.m

--- BigDots_ERP_analysis.m
+++ BigDots_ERP_analysis.m
@@ -3,7 +3,8 @@ close all
 clc
 chanlocs = readlocs('cap64.loc');
 
-path_temp = 'D:\Participant Folders_new\'; %TCD Laptop
+% path_temp = 'D:\Participant Folders_new\'; %TCD Laptop
+path_temp = 'S:\R-MNHS-SPP\Bellgrove-data\4. Dan Newman\Participant Folders_new\'; %Monash PC
 % save_path = 'D:\Participant Folders_new\';
 
 %% Create paths for subjects
